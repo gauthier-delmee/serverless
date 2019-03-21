bc783fddf2c0ff3d21ebbea7deff8df262c04ad7
Gauthier Delmee <gauthier.delmee@cyara.com>
2019-03-21 16:25:50 +1100

    refactored the insert endpoint to use the dynamodb backend

--- insert/main.go
+++ insert/main.go
@@ -3,61 +3,62 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+// Movie structure
 type Movie struct {
-	ID   int    `json:"id"`
+	ID   string `json:"id"`
 	Name string `json:"name"`
 }
 
-var movies = []Movie{
-	Movie{
-		ID:   1,
-		Name: "Avengers",
-	},
-	Movie{
-		ID:   2,
-		Name: "Ant-Man",
-	},
-	Movie{
-		ID:   3,
-		Name: "Thor",
-	},
-	Movie{
-		ID:   4,
-		Name: "Doctor Strange",
-	},
-}
-
-func insert(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func insert(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	var movie Movie
-	err := json.Unmarshal([]byte(req.Body), &movie)
+	err := json.Unmarshal([]byte(request.Body), &movie)
 	if err != nil {
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusBadRequest,
 			Body:       "Invalid payload",
 		}, nil
 	}
-
-	movies = append(movies, movie)
-
-	response, err := json.Marshal(movies)
+	cfg, err := external.LoadDefaultAWSConfig()
 	if err != nil {
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusInternalServerError,
-			Body:       err.Error(),
+			Body:       "Error while retrieving AWS credentials",
 		}, nil
 	}
-
+	svc := dynamodb.New(cfg)
+	req := svc.PutItemRequest(&dynamodb.PutItemInput{
+		TableName: aws.String(os.Getenv("TABLE_NAME")),
+		Item: map[string]dynamodb.AttributeValue{
+			"ID": dynamodb.AttributeValue{
+				S: aws.String(movie.ID),
+			},
+			"Name": dynamodb.AttributeValue{
+				S: aws.String(movie.Name),
+			},
+		},
+	})
+	_, err = req.Send()
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body:       "Error while inserting movie to DynamoDB",
+		}, nil
+	}
 	return events.APIGatewayProxyResponse{
 		StatusCode: http.StatusOK,
 		Headers: map[string]string{
 			"Content-Type": "application/json",
 		},
-		Body: string(response),
 	}, nil
 }
 
