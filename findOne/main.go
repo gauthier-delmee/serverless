7eaed3f406aa42437ab1602aa07c60f4021c4ca4
Gauthier Delmee <gauthier.delmee@cyara.com>
2019-03-21 16:01:52 +1100

    Updated the findOne endpoint to use backend

--- findOne/main.go
+++ findOne/main.go
@@ -3,47 +3,56 @@ package main
 import (
 	"encoding/json"
 	"net/http"
-	"strconv"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-var movies = []struct {
-	ID   int    `json:"id"`
+// Movie structure
+type Movie struct {
+	ID   string `json:"id"`
 	Name string `json:"name"`
-}{
-	{
-		ID:   1,
-		Name: "Avengers",
-	},
-	{
-		ID:   2,
-		Name: "Ant-Man",
-	},
-	{
-		ID:   3,
-		Name: "Thor",
-	},
-	{
-		ID:   4,
-		Name: "Doctor Strange",
-	},
 }
 
-func findOne(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	id, err := strconv.Atoi(req.PathParameters["id"])
+func findOne(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	id := request.PathParameters["id"]
+	cfg, err := external.LoadDefaultAWSConfig()
 	if err != nil {
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusInternalServerError,
-			Body:       "ID must be a number",
+			Body:       "Error while retrieving AWS credentials",
 		}, nil
 	}
-	response, err := json.Marshal(movies[id-1])
+	svc := dynamodb.New(cfg)
+	req := svc.GetItemRequest(&dynamodb.GetItemInput{
+		TableName: aws.String(os.Getenv("TABLE_NAME")),
+		Key: map[string]dynamodb.AttributeValue{
+			"ID": dynamodb.AttributeValue{
+				S: aws.String(id),
+			},
+		},
+	})
+	res, err := req.Send()
 	if err != nil {
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusInternalServerError,
-			Body:       err.Error(),
+			Body:       "Error while fetching movie from DynamoDB",
+		}, nil
+	}
+	movie := Movie{
+		ID:   *res.Item["ID"].S,
+		Name: *res.Item["Name"].S,
+	}
+	response, err := json.Marshal(movie)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body:       "Error while decoding to string value",
 		}, nil
 	}
 	return events.APIGatewayProxyResponse{
